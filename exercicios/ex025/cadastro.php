a63571be4b746b43590229c15a2e46d6f7c4207d
LucasFonseca24 <lucas.fonseca.2444@gmail.com>
2022-07-28 15:14:53 -0300

    att

---
+++ exercicios/ex025/cadastro.php
@@ -0,0 +1,20 @@
+os dados foram enviados
+ja que vc não sabe PHP perdeu os dados
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        *{
+            background-color:"white";
+        }
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
